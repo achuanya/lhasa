52154a7cfc78e9b9ebd812f8b55cfb8512e770db
achuanya <haibao1027@gmail.com>
2025-05-19 01:35:16 +0800

    update

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://lhasa.icu/", // replace this with your deployed domain
+  website: "https://blog.lhasa.icu/", // replace this with your deployed domain
   author: "游钓四方",
   profile: "https://satnaing.dev/",
   desc: "千禧年小孩，长途骑行小学生、野钓路亚、振出并继、古典乐、茶叶爱好者",
-  title: "lhasa",
+  title: "游钓四方",
   ogImage: "https://cos.lhasa.icu/StylePictures/my-photo.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
